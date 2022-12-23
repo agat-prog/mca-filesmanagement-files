9b55fe51787317b331435317588567d54217879d
agat-prog <agatalba@gmail.com>
2022-12-23 15:19:48 +0100

    cambios

---
+++ files-api/src/main/resources/db/migration/V2__update_schema.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO `INIT_OPTION` SET `ID` = 1, `CODE` = 'SEDE', `DESCRIPTION` = 'Sede electrónica';
+INSERT IGNORE INTO `INIT_OPTION` SET `ID` = 2, `CODE` = 'PETICION_DTO', `DESCRIPTION` = 'A petición departamento';
